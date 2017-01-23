0c7ec1f155ecd1a5bc01e0c274bbbc6ce84122dd
AdamJOtto <aotto@ualberta.ca>
2017-01-23 16:17:51 -0700

    Added Signals to findpattern.c

--- findpattern.c
+++ findpattern.c
@@ -1,9 +1,18 @@
 #include "findpattern.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <setjmp.h>
 #include <signal.h>
 
-int main() {
+int determineIfReadWriteAddressLocation(char *);
+void initiate_handler();
+void restore_handler();
+void handler(int);
+
+static sigjmp_buf signalBuffer;
+static struct sigaction newSignalHandler, oldSignalHandler;
+
+int main(int argc, char *argv[]) {
 	
 	/*For testing purposes -------*/
 	unsigned char *testPattern = (char*)'f';
@@ -26,13 +35,28 @@ unsigned int findpattern(unsigned char *pattern, unsigned int patlength, struct 
 	char *locationOfWherePatternStarts = currentAddress;
 	char data = ' ';
 	
-	for(currentPage = 0; currentPage < pageTotal; currentPage++)
+	for(currentPage = 0; currentPage < /*pageTotal*/100; currentPage++)
 	{
-		printf("current address: 0x%x", (int)currentAddress);
-		data = currentAddress++;
-		printf(" current data: %c\n",data);
+		printf("Start switch.\n");
+		int hold = determineIfReadWriteAddressLocation(currentAddress);
+		printf("hold is: %d", hold);
+		switch (hold)
+		{
+			//-1 means not read/write
+			case -1: currentAddress++; continue; break;
+			//0 means read, not write
+			case 0:
+				data = *currentAddress++; printf(" current data: %c\n",data); break;
+			//1 means read and write
+			case 1:
+				printf("attempting to read from address...");
+				data = *currentAddress++;
+				printf(" current data: %c\n",data);
+				break;
+		}
+		printf("Current data: %c\n",data);
 
-		if(data == &pattern)
+		if(data == *pattern)
 		{
 			printf("Pattern found...\n");
 			patternCount++;
@@ -49,3 +73,65 @@ unsigned int findpattern(unsigned char *pattern, unsigned int patlength, struct 
 		currentAddress++;
 	}
 }
+
+
+int determineIfReadWriteAddressLocation(char * address)
+{
+	//printf("start initiate_handler.\n");
+	initiate_handler();
+	//printf("start sigsetjmp.\n");
+	int SignalValue = sigsetjmp(signalBuffer, 1);
+	printf("SignalValue: %d\n", SignalValue);
+	if ( SignalValue != 0)
+	{
+		//Cant read address.
+		printf("cant read address.\n");
+		restore_handler();
+		return -1;
+	}
+
+	SignalValue = sigsetjmp(signalBuffer, 1);
+	printf("SignalValue: %d\n", SignalValue);
+	if ( SignalValue != 0)
+	{
+		//Cant write to address.
+		printf("cant write address, but can read.\n");
+		restore_handler();
+		return 0;
+	}
+
+	printf("can read and write to address.\n");
+	restore_handler();
+	printf("returning to findpattern...\n");
+	return 1;
+}
+
+
+
+void initiate_handler() {
+	newSignalHandler.sa_handler = handler;
+	sigemptyset(&newSignalHandler.sa_mask);
+	newSignalHandler.sa_flags = 0;
+	if (sigaction(SIGSEGV, NULL, &oldSignalHandler) == -1)
+	{
+		err(1, "Cannot save previous sigaction.\n");
+	}
+	if (sigaction(SIGSEGV, &newSignalHandler, NULL) == -1)
+	{
+		err(1, "Cannot set next sigaction.\n");
+	}	
+}
+
+
+void restore_handler() {
+	if (sigaction(SIGSEGV, &oldSignalHandler, NULL) == -1) {
+		err(1, "Cannot restore previous signal handler.\n");
+	}
+}
+
+
+void handler(int signo) {
+	siglongjmp(signalBuffer, 1);
+	printf("Does this print?");
+}
+
