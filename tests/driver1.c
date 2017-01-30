9764a31872d328dbdc6b7be6bbcd232a1e23dd97
AdamJOtto <aotto@ualberta.ca>
2017-01-30 16:53:23 -0700

    Skeleton for driver 1.

--- tests/driver1.c
+++ tests/driver1.c
@@ -7,13 +7,66 @@ unsigned int PAGE_SIZE = getpagesize();
 
 int main() {
 
-    unsigned char *pattern = (char *) 'A';
-    unsigned int patlength = 1;
+	unsigned char *pattern = (char *) 'A';
+	unsigned int patlength = 2;
 
-    struct patmatch locations[10];
-    unsigned int loclength = sizeof(locations) / sizeof(patmatch);
+	struct patmatch locations[10];
+	unsigned int loclength = sizeof(locations) / sizeof(patmatch);
+	
+	
+	printf("Test 1:\n");
+	unsigned int number = findpattern(pattern, patlength, locations, loclength)
+	
+	//First test	
+	printf("Pass 1\nTotal Matches: %d\n", number);
+	int i;	
+	for (i = 0; i < loclength; i++)
+	{
+		if(locations[i].location != NULL && locations[i].mode != NULL)
+		{
+			printf("%d\t%c",locations[i].location, locations[i].mode=='0'?"MEM_RW":"MEM_RO");
+		}			
+	}
+	
+	//This address may need to be changed later if its read only.
+	int *address = 0xaaaaaaaa;
+	for(i = 0; i < patlength; i++)
+	{
+		*address = 'A';
+		address++;
+	}
+	
+	unsigned char *pattern2 = (char *) 'A';
+	unsigned int patlength2 = 2;
 
-    unsigned int number = findpattern(pattern, patlength, locations, loclength)
-    printf(number);
+	struct patmatch locations2[10];
+	unsigned int loclength2 = sizeof(locations) / sizeof(patmatch);	
+	
+	//Second test with changes.
+	unsigned int number = findpattern(pattern2, patlength2, locations2, loclength2)
 
+	printf("Pass 2\nTotal Matches: %d\n", number);
+	
+	int i;	
+	for (i = 0; i < loclength; i++)
+	{
+		if(locations[i].location != NULL && locations[i].mode != NULL)
+		{
+			printf("%d\t%c\t%c",locations2[i].location, locations2[i].mode=='0'?"MEM_RW":"MEM_RO",
+				(locations2[i].mode != NULL && locations[i] == NULL)?
+					"N"
+					:
+					( locations2[i].mode == locations[i].mode ? "U":"C")
+			);
+		}			
+	}
+	
 }
+
+
+
+
+
+
+
+
